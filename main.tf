c20b28a38694123b8bf61d0217b1eed74167745e
BorislavTsanovsi <emptymas@gmail.com>
2025-04-06 22:05:17 +0300

    added tfstaate with neccessery cnages

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.25.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "StorageRG"
+    storage_account_name = "taskboardstoragesomename"
+    container_name = "taskboardcontainersomename"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
